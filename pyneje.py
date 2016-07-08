307e698f6b34f70d5b4e4eba8364a009a0f2ca51
Fredrik Andersson <fredrikandersson@mac.com>
2016-07-08 04:19:39 +0200

    Added missing dependencies

---
+++ pyneje.py
@@ -0,0 +1,41 @@
+import Tkinter
+import os
+import platform
+import subprocess
+import time
+import hal.engraver
+import ui_widgets.serialdialog
+
+
+# Hack from stackoverflow to raise the window on most platforms.
+from pyneje_gui.main import GUI
+
+
+def raise_app(root_tk):
+    root_tk.attributes("-topmost", True)
+    if platform.system() == 'Darwin':
+        tmpl = 'tell application "System Events" to set frontmost of every process whose unix id is {} to true'
+        script = tmpl.format(os.getpid())
+        output = subprocess.check_call(['/usr/bin/osascript', '-e', script])
+    root_tk.after(0, lambda: root_tk.attributes("-topmost", False))
+
+
+if __name__ == "__main__":
+    root = Tkinter.Tk()
+    raise_app(root)
+    root.update()
+
+    if False:
+        ui = GUI(root, None).run()
+    else:
+        serialPort = ui_widgets.serialdialog.SerialSelection(root, hal.engraver.get_serial_ports()).returnValue
+
+        if serialPort is None:
+            print "Can't operate without a valid serial port"
+            exit(1)
+
+        device = hal.engraver.Engraver(serialPort)
+        device.reset()
+        ui = GUI(root, device).run()
+
+        device.disconnect()
---
+++ ui_widgets/serialdialog.py
@@ -0,0 +1,25 @@
+import Tkinter
+
+from tkSimpleDialog import tkSimpleDialog
+
+
+class SerialSelection(tkSimpleDialog.Dialog):
+    def __init__(self, parent, serial_list, callback=None, title=None):
+        self.dropdown_selection = None
+        self.serial_list = serial_list
+        self.returnValue = None
+        tkSimpleDialog.Dialog.__init__(self, parent, callback, title)
+
+    def body(self, master):
+        if len(self.serial_list) > 0:
+            Tkinter.Label(master, text="Select the hal device:").grid(row=0)
+            self.dropdown_selection = Tkinter.StringVar(master)
+            self.dropdown_selection.set(self.serial_list[0])
+            return Tkinter.OptionMenu(master, self.dropdown_selection, *self.serial_list).grid()
+        else:
+            return Tkinter.Label(master, text="No suitable devices detected.").grid(row=0)
+
+    def apply(self):
+        if self.dropdown_selection:
+            self.returnValue = self.dropdown_selection.get()
+
